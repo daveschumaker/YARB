f58e480ef89d8b1f881a10720215965207bb7eb1
Dave Schumaker <daveschumaker@gmail.com>
2016-08-14 20:22:09 -0700

    Add tests and remove packages used in another project.

---
+++ test/index_test.js
@@ -0,0 +1,9 @@
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+describe('index.js', function() {
+    it('first test should resolve to true', function() {
+        var test = true;
+        test.should.equal(true);
+    });
+});
